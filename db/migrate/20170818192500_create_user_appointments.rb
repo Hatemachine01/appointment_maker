801e5560b8d43fcb2f3fa9ffb131ed6d7c43829b
Julio <juliodevasquez@gmail.com>
2017-08-31 18:38:07 -0500

    ADDED CASCADE

--- db/migrate/20170818192500_create_user_appointments.rb
+++ db/migrate/20170818192500_create_user_appointments.rb
@@ -2,7 +2,7 @@ class CreateUserAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :user_appointments do |t|
       t.references :user, foreign_key: true
-      t.references :appointment, foreign_key: true
+      t.references :appointment, foreign_key: {on_delete: :cascade}
 
       t.timestamps
     end
